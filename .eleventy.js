90cc6947b0177f8a62271d93d7da6a1cda392166
pkgamma <pkgamma@gmail.com>
2018-12-23 17:44:56 -0600

    test refierect

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.setTemplateFormats("html,liquid,njk,css,js,eot,ttf,woff,svg,jpg,png,cur");
+  eleventyConfig.addPassthroughCopy("_redirects");
 };
